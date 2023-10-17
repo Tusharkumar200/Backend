50645e8a27ee3922f931614a078b26916987fbd4
Tushar kumar <tusharnwd2003@gmail.com>
2023-10-17 17:30:55 +0530

    final

--- javascript/Day_15/script.js
+++ javascript/Day_15/script.js
@@ -3,23 +3,37 @@ const calculateTemp = ()=>{
 
         const tempSelected = document.getElementById('temp_diff');
         const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
-        console.log(valueTemp);
+       
 
         const celToFah = (cel) =>{
-                let fahrenheit = Math.round((cel * 9/5) +32);
+
+                // (°C × 9/5) + 32 = °F
+
+
+                let fahrenheit = Math.round((cel * 9/5)+32);
                 return fahrenheit;
         }
 
-        let result
+        const fahToCel = (f) =>{
+
+                //  (°F − 32) × 5/9 = °C
+
+                let celsius = Math.round((f-32) * 5/9);
+                return celsius;
+        }
+
+        let result;
 
         if(valueTemp =='cel'){
                 result = celToFah(numberTemp);
                 
-                document.getElementById('resultContainer').innerHTML= '${result}°Fahrenheit';
+                
+                document.getElementById('resultContainer').innerHTML= `= ${result}°Fahrenheit`;
         }
         else{
                 result = fahToCel(numberTemp);
-                document.getElementById('resultContainer').innerHTML= '${result}°Celsius';
+               document.getElementById('resultContainer').innerHTML= `= ${result}°Celsius`;
+                
         }
 
 }

acfa48952d1c3e341c3494feb4e211c3ccba60e9
Tushar kumar <tusharnwd2003@gmail.com>
2023-10-18 00:03:26 +0530

    node started

---
+++ nodeJS/first.js
@@ -0,0 +1,33 @@
+
+
+const imp_exp = require('./imp_exp')
+
+console.log("Now start with nodeJS");
+
+//                  Basic of JS
+
+//  To create a variables
+// let a= 10;
+// let b = 20;
+// console.log(a+b);
+
+// To Create a Array
+
+let a = [5,48,6,48,2,45,1,];
+
+// console.log(a);
+
+
+console.log(imp_exp);
+
+let first = imp_exp.x;
+let second = imp_exp.y;
+
+console.log(first + second);
+
+console.log("This is the path of by dir "+__dirname);
+console.log("This is the path of by file "+__filename);
+
+
+
+

51b5c92aec593c6c0174d774fe95004d2946deac
Tushar kumar <tusharnwd2003@gmail.com>
2023-10-24 23:24:58 +0530

    item_inserted

--- nodeJS/Day_24/insertData.js
+++ nodeJS/Day_24/insertData.js
@@ -1,12 +1,13 @@
 const dbConnect = require('./mongodb');
 
-const insert =async ()=>{
+const insert = async () => {
     const db = await dbConnect();
-    const result = db.insert(
+    const result = db.insertOne(
         {
-            name:"Raj",
-            age: 21,
-            city:"Mumbai"
+            "owner": "Technical Thapa",
+            "channelName": "Thapa Technical",
+            "subscriber": 1000000,
+            "brand": "google"
         })
-}   
+}
 insert();

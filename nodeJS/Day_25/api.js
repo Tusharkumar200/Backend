ac26158bc1772559ba96928bf2a80c9116d9ec5f
Tushar kumar <tusharnwd2003@gmail.com>
2023-10-26 14:54:49 +0530

    update_and_delete

--- nodeJS/Day_25/api.js
+++ nodeJS/Day_25/api.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const dbconnect = require('./mongodb');
+const mongodb = require('mongodb')
 const app = express();
 
+// important line
 app.use(express.json());
 
+// get Method does't contain body 
+
+// To read the data of the database
 app.get('/',async(req,resp)=>{
     const data = await dbconnect();
     const result = await data.find().toArray();
@@ -11,11 +16,29 @@ app.get('/',async(req,resp)=>{
     resp.send(result);
 })
 // USING POSTMAN
+// inserting Data into DataBase
 app.post('/',async(req,resp)=>{
-    console.log(req.body);
     let data = await dbconnect();
     let result = await data.insertOne(req.body);
     resp.send(result);
 })
+
+// update The data of the DataBase
+app.put('/:xyz',async(req,resp)=>{  // we can update the owner
+        let data = await dbconnect();
+        let result = await data.updateOne({owner:req.params.xyz},{$set:req.body});
+        
+        resp.send({result:"update"});
+})
+
+// http://localhost:5000/6538f2ee683d47cb17e4d4d2
+//  import const mongodb = require('mongodb');
+app.delete('/:id',async(req,resp)=>{
+    console.log(req.params.id)
+    const data = await dbconnect();
+    let result = await data.deleteOne({_id: new mongodb.ObjectId(req.params.id)});
+    resp.send(result);
+})
+
 
 app.listen(5000);

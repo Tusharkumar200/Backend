825e8624b55914592852a7e1d02f6c0727f158e8
Tushar kumar <tusharnwd2003@gmail.com>
2023-10-28 11:21:37 +0530

    created_student_schema

--- nodeJS/Day_26/mongoose.js
+++ nodeJS/Day_26/mongoose.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose')
 
 const main = async ()=>{
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    // await mongoose.connect('mongodb://127.0.0.1:27017/test'); 
+    await mongoose.connect('mongodb://127.0.0.1:27017/student'); 
     const ProductSchema = new mongoose.Schema({
-        owner: String,
-        regNo: Number
+    
+        name: String,
+        branch: String,
+        
     });
-    const ProductModel = mongoose.model('YouTube',ProductSchema);
-    let data = new ProductModel({owner: "sachin",regNo:604})
+    const ProductModel = mongoose.model('student',ProductSchema);
+    let myData = {Name:"Tushar kumar",Branch:"CSE"}
+    let data = new ProductModel({myData})
     let result= await data.save();
     console.log(result);
 }
--- nodeJS/Day_27/CRUD_with_mongoose.js
+++ nodeJS/Day_27/CRUD_with_mongoose.js
@@ -2,38 +2,43 @@ const mongoose = require('mongoose');
 
 // Define the schema for the youtubes collection
 const ProductSchema = new mongoose.Schema({
+    
     name: String,
-    branch: String
+    branch: String,
+   
 });
 
 // Define the model for the youtubes collection
-// const ProductModel = mongoose.model('youtubes', ProductSchema);
-const ProductModel = mongoose.model('youtubes', ProductSchema);
+
+//  const ProductModel = mongoose.model('youtubes', ProductSchema);
+ const ProductModel = mongoose.model('student', ProductSchema);
 
 // Update a document in the youtubes collection
 async function updateProduct() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
-    let data = await ProductModel.updateOne({ name: "sachin" }, { $set:{ branch: "CSE" } });
+    await mongoose.connect('mongodb://127.0.0.1:27017/student');
+    let myData = {Name:"Tushar kumar",Branch:"CSE"}
+    let data = await ProductModel.updateOne({ name:"Tushar Kumar" }, { $set:{ myData } });
     console.log(data);
 }
 
-// updateProduct();
+// updateProduct(); 
 
 
 //  Delete the data
 
 const deleteInDB = async () => {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
-    let data = await ProductModel.deleteOne({ name: "sachin" });
+    await mongoose.connect('mongodb://127.0.0.1:27017/student');
+    let data = await ProductModel.deleteOne({ name:"Tushar Kumar" });
     console.log(data);
 }
-// deleteInDB();
+deleteInDB();
 
+// Read the Data
 const findInDB = async () => {
     await mongoose.connect('mongodb://127.0.0.1:27017/test');
     let data = await ProductModel.find();
     console.log(data);
 }
 
-findInDB();
+// findInDB();
 

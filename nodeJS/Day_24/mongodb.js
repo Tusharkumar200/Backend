d0ce4ca7b6d9e8e22a48cba7b0a65bcea18d40e9
Tushar kumar <tusharnwd2003@gmail.com>
2023-10-29 10:49:04 +0530

    commite

--- nodeJS/Day_24/mongodb.js
+++ nodeJS/Day_24/mongodb.js
@@ -1,12 +1,14 @@
 const {MongoClient} = require('mongodb');
 const url = 'mongodb://127.0.0.1:27017';
-const databaseName = 'test'
+// const databaseName = 'test'
+const databaseName = 'e-com'
 const client = new MongoClient(url);
 
 async function dbConnect(){
     let result = await client.connect();
     let db = result.db(databaseName);
-    return db.collection('YouTube'); 
+    // return db.collection('YouTube'); 
+    return db.collection('product'); 
 }
 dbConnect();
 module.exports = dbConnect; 

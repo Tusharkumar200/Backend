6636c73a5f3dfe5cbf0b9b37375e8605224fbac1
Tushar kumar <tusharnwd2003@gmail.com>
2023-11-03 22:49:38 +0530

    array_practice

---
+++ practice/array.js
@@ -0,0 +1,17 @@
+// practice: array
+
+const myArray = ['Tushar', 'Ankit', 'Sachin', 'Mohit', 'Harsh'];
+
+const result = myArray[1];
+console.log(result);
+
+// To print the length of the array
+const lastElement = myArray.length;
+console.log(lastElement);
+
+// to print all the element
+
+for(let i = 0; i<lastElement;i++){
+    console.log(myArray[i]);
+}
+

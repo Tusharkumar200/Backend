1d4adef764d14bc4d8f4fe2e9931a84d399382aa
Tushar kumar <tusharnwd2003@gmail.com>
2023-11-18 21:50:28 +0530

    improvement

--- projects/sign_in_and_sign up page/src/index.js
+++ projects/sign_in_and_sign up page/src/index.js
@@ -15,8 +15,8 @@ app.use(express.urlencoded({extended:false}))
 
 
 app.get('/', (req,res)=>{
-    res.render('login')
-    style:'home.css'
+    res.render('signup')
+   
 })
 
 app.get('/signup', (req,res)=>{

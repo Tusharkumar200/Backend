1ee9992a6679d045202a6de0e0d75492af04aec4
Tushar kumar <tusharnwd2003@gmail.com>
2023-11-26 14:42:46 +0530

    committed

---
+++ reactJS/Day_54/React_Projects/TO-DO List_v1.0/src/components/TodoItem1.jsx
@@ -0,0 +1,20 @@
+ function TodoItem1() {
+  return;
+  <div class="container text-center p-2">
+    <div class="row">
+      <div class="col-6">
+        <p> Milk and Eggs</p>
+      </div>
+      <div class="col-4">
+        <p>10/12/23</p>
+      </div>
+      <div class="col-2">
+        <button type="button" class="btn btn-danger">
+          Delete
+        </button>
+      </div>
+    </div>
+  </div>;
+}
+
+export default TodoItem1;
---
+++ reactJS/Day_54/React_Projects/TO-DO List_v1.0/src/components/TodoItem2.jsx
@@ -0,0 +1,18 @@
+export function TodoItem2(){
+  return 
+  <div class="container text-center" p-2>
+  <div class="row">
+    <div class="col-6">
+    <p>GoTo Collage </p>
+    </div>
+    <div class="col-4 -m 2px">
+    <p>11/12/23</p>
+    </div>
+    <div class="col-2">
+    <button type="button" class="btn btn-danger">Delete</button>
+    </div>
+  </div>
+</div>
+
+
+} 
